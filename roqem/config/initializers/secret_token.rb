eb5d60b747ff1786ab641b0eec3d1d91c1814d2f
Elliot Nash <Elliot@venturemedias.com>
2012-07-22 23:14:55 -0400

    initial commit

---
+++ roqem/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roqem::Application.config.secret_token = 'd5aa7da451d70f150a94e64768cec8467998f75a4af5f7c5a2f25bb58906b7c8f1954fdbb51889b0e313ba281d14c17848a57a4288a905b816aaab27b4aa0236'
